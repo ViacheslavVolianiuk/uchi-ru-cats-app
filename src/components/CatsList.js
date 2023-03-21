4343f8c1a303725f5dd0ee9c650940f97415c2f8
Viacheslav Volianiuk <78928033+ViacheslavVolianiuk@users.noreply.github.com>
2023-03-21 16:58:06 +0400

    Update CatsList.js

--- src/components/CatsList.js
+++ src/components/CatsList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import CatCard from './CatCard';
 import './CatsList.css';
 import { v4 as uuidv4 } from 'uuid';
@@ -6,21 +6,8 @@ import { v4 as uuidv4 } from 'uuid';
 function CatsList() {
   const [catsData, setCatsData] = useState([]);
   const [isBottom, setIsBottom] = useState(false);
-
-  // Use a ref to keep track of the last cat card element
+  const [lastCardIndex, setLastCardIndex] = useState(0);
   const lastCatCardRef = useRef(null);
-
-  const handleIntersection = useCallback(
-    (entries) => {
-      entries.forEach((entry) => {
-        // If the element is visible, fetch more cats
-        if (entry.isIntersecting) {
-          setIsBottom(true);
-        }
-      });
-    },
-    [setIsBottom]
-  );
 
   useEffect(() => {
     fetch('https://api.thecatapi.com/v1/images/search?limit=40')
@@ -28,6 +15,7 @@ function CatsList() {
       .then((data) => {
         data.forEach((item) => (item.liked = false));
         setCatsData(data);
+        setLastCardIndex(data.length - 1);
       });
   }, []);
 
@@ -39,45 +27,46 @@ function CatsList() {
       .then((data) => {
         data.forEach((item) => (item.liked = false));
         setCatsData((prevCatsData) => [...prevCatsData, ...data]);
+        setLastCardIndex(prevLastCardIndex => prevLastCardIndex + data.length);
       });
 
     setIsBottom(false);
   }, [isBottom]);
 
   useEffect(() => {
-    // Create a new IntersectionObserver instance
-    const observer = new IntersectionObserver(handleIntersection);
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsBottom(true);
+        }
+      });
+    });
 
-    // Observe the last cat card element
     if (lastCatCardRef.current) {
       observer.observe(lastCatCardRef.current);
     }
 
-    // Cleanup function to disconnect observer
     return () => {
       observer.disconnect();
     };
-  }, [handleIntersection, lastCatCardRef]);
+  }, [lastCardIndex]);
 
   return (
     <div className="cats-list">
-      {catsData &&
-        catsData.map((item, index) => {
-          if (index === catsData.length - 1) {
-            return (
-              <CatCard
-                key={uuidv4()}
-                ref={lastCatCardRef}
-                url={item.url}
-                liked={item.liked}
-              />
-            );
-          } else {
-            return (
-              <CatCard key={uuidv4()} url={item.url} liked={item.liked} />
-            );
-          }
-        })}
+      {catsData.map((item, index) => {
+        if (index === lastCardIndex) {
+          return (
+            <CatCard
+              key={uuidv4()}
+              ref={lastCatCardRef}
+              url={item.url}
+              liked={item.liked}
+            />
+          );
+        } else {
+          return <CatCard key={uuidv4()} url={item.url} liked={item.liked} />;
+        }
+      })}
     </div>
   );
 }
