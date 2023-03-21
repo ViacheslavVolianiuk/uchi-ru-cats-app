ee89e992a5da37e442e11e608765d333dcc9b951
Viacheslav Volianiuk <78928033+ViacheslavVolianiuk@users.noreply.github.com>
2023-03-21 17:40:17 +0400

    Update CatsList.js

--- src/components/CatsList.js
+++ src/components/CatsList.js
@@ -3,79 +3,60 @@ import CatCard from './CatCard';
 import './CatsList.css';
 import { v4 as uuidv4 } from 'uuid';
 
-function CatsList({ perPage }) {
+function CatsList() {
   const [catsData, setCatsData] = useState([]);
-  const [isBottom, setIsBottom] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
   const [page, setPage] = useState(1);
 
-  // Use a ref to keep track of the last cat card element
-  const lastCatCardRef = useRef(null);
+  const observer = useRef();
+  const lastCatCardRef = useCallback(
+    node => {
+      if (isFetching) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting) {
+          setPage(prevPage => prevPage + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [isFetching]
+  );
 
   const fetchCats = useCallback(() => {
-    fetch(`https://api.thecatapi.com/v1/images/search?limit=${perPage}&page=${page}`)
+    setIsFetching(true);
+    fetch(`https://api.thecatapi.com/v1/images/search?limit=10&page=${page}`)
       .then((res) => res.json())
       .then((data) => {
         data.forEach((item) => (item.liked = false));
         setCatsData((prevCatsData) => [...prevCatsData, ...data]);
-        setPage((prevPage) => prevPage + 1);
-      })
-      .catch((error) => {
-        console.log('Error fetching cats:', error);
+        setIsFetching(false);
       });
-  }, [perPage, page]);
+  }, [page]);
 
   useEffect(() => {
     fetchCats();
   }, [fetchCats]);
 
-  useEffect(() => {
-    if (!isBottom) return;
-
-    fetchCats();
-    setIsBottom(false);
-  }, [isBottom, fetchCats]);
-
-  useEffect(() => {
-    // Create a new IntersectionObserver instance
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        // If the element is visible, fetch more cats
-        if (entry.isIntersecting) {
-          setIsBottom(true);
-        }
-      });
-    });
-
-    // Observe the last cat card element
-    if (lastCatCardRef.current) {
-      observer.observe(lastCatCardRef.current);
-    }
-
-    // Cleanup function to disconnect observer
-    return () => {
-      observer.disconnect();
-    };
-  }, [lastCatCardRef]);
-
   return (
     <div className="cats-list">
-      {catsData &&
-        catsData.map((item, index) => {
-          if (index === catsData.length - 1) {
-            return (
-              <CatCard
-                key={uuidv4()}
-                ref={lastCatCardRef}
-                url={item.url}
-                liked={item.liked}
-              />
-            );
-          } else {
-            return (
-              <CatCard key={uuidv4()} url={item.url} liked={item.liked} />
-            );
-          }
-        })}
+      {catsData.map((item, index) => {
+        if (catsData.length === index + 1) {
+          return (
+            <CatCard
+              key={uuidv4()}
+              ref={lastCatCardRef}
+              url={item.url}
+              liked={item.liked}
+            />
+          );
+        } else {
+          return (
+            <CatCard key={uuidv4()} url={item.url} liked={item.liked} />
+          );
+        }
+      })}
+      {isFetching && <div>Loading...</div>}
     </div>
   );
 }
