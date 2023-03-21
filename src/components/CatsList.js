8a142a774b299dea8b37e7b9233180e8a27edce9
Viacheslav Volianiuk <78928033+ViacheslavVolianiuk@users.noreply.github.com>
2023-03-21 17:49:09 +0400

    Update CatsList.js

--- src/components/CatsList.js
+++ src/components/CatsList.js
@@ -1,36 +1,18 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import CatCard from './CatCard';
 import './CatsList.css';
 import { v4 as uuidv4 } from 'uuid';
 
 function CatsList() {
   const [catsData, setCatsData] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
   const [page, setPage] = useState(1);
 
-  const observer = useRef();
-  const lastCatCardRef = useCallback(
-    node => {
-      if (isFetching) return;
-      if (observer.current) observer.current.disconnect();
-      observer.current = new IntersectionObserver(entries => {
-        if (entries[0].isIntersecting) {
-          setPage(prevPage => prevPage + 1);
-        }
-      });
-      if (node) observer.current.observe(node);
-    },
-    [isFetching]
-  );
-
   const fetchCats = useCallback(() => {
-    setIsFetching(true);
     fetch(`https://api.thecatapi.com/v1/images/search?limit=10&page=${page}`)
       .then((res) => res.json())
       .then((data) => {
         data.forEach((item) => (item.liked = false));
         setCatsData((prevCatsData) => [...prevCatsData, ...data]);
-        setIsFetching(false);
       });
   }, [page]);
 
@@ -38,25 +20,30 @@ function CatsList() {
     fetchCats();
   }, [fetchCats]);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop
+        === document.documentElement.offsetHeight
+      ) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  useEffect(() => {
+    fetchCats();
+  }, [page, fetchCats]);
+
   return (
     <div className="cats-list">
-      {catsData.map((item, index) => {
-        if (catsData.length === index + 1) {
-          return (
-            <CatCard
-              key={uuidv4()}
-              ref={lastCatCardRef}
-              url={item.url}
-              liked={item.liked}
-            />
-          );
-        } else {
-          return (
-            <CatCard key={uuidv4()} url={item.url} liked={item.liked} />
-          );
-        }
-      })}
-      {isFetching && <div>Loading...</div>}
+      {catsData &&
+        catsData.map((item) => (
+          <CatCard key={uuidv4()} url={item.url} liked={item.liked} />
+        ))}
     </div>
   );
 }
