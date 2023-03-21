22092c22f7e7c5ae2f76d434331056a9f58bfa2c
Viacheslav Volianiuk <78928033+ViacheslavVolianiuk@users.noreply.github.com>
2023-03-21 17:08:22 +0400

    Update CatsList.js

--- src/components/CatsList.js
+++ src/components/CatsList.js
@@ -5,82 +5,90 @@ import { v4 as uuidv4 } from 'uuid';
 
 function CatsList() {
   const [catsData, setCatsData] = useState([]);
-  const [isBottom, setIsBottom] = useState(false);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
 
-  // Use a ref to keep track of the second-last cat card element
-  const secondLastCatCardRef = useRef(null);
+  const observer = useRef(null);
+  const lastCatCardRef = useRef(null);
 
   useEffect(() => {
-    fetch('https://api.thecatapi.com/v1/images/search?limit=40')
-      .then((res) => res.json())
-      .then((data) => {
-        data.forEach((item) => (item.liked = false));
-        setCatsData(data);
-      });
+    fetchCats();
   }, []);
 
   useEffect(() => {
-    if (!isBottom) return;
+    if (!isLoading) return;
+    fetchCats();
+  }, [isLoading]);
 
-    fetch('https://api.thecatapi.com/v1/images/search?limit=40')
+  useEffect(() => {
+    observer.current = new IntersectionObserver(
+      (entries) => {
+        const target = entries[0];
+        if (target.isIntersecting) {
+          setPage((page) => page + 1);
+          setIsLoading(true);
+        }
+      },
+      { threshold: 1 }
+    );
+  }, []);
+
+  useEffect(() => {
+    if (lastCatCardRef.current) {
+      observer.current.observe(lastCatCardRef.current);
+    }
+    return () => {
+      observer.current.disconnect();
+    };
+  }, [catsData]);
+
+  const fetchCats = () => {
+    fetch(`https://api.thecatapi.com/v1/images/search?limit=10&page=${page}`)
       .then((res) => res.json())
       .then((data) => {
         data.forEach((item) => (item.liked = false));
         setCatsData((prevCatsData) => [...prevCatsData, ...data]);
-      });
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  };
 
-    setIsBottom(false);
-  }, [isBottom]);
-
-  useEffect(() => {
-    // Create a new IntersectionObserver instance
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        // If the second-last cat card is visible, fetch more cats
-        if (entry.target === secondLastCatCardRef.current) {
-          const { offsetHeight } = document.documentElement;
-          const { scrollHeight, scrollTop } = document.body;
-          if (scrollTop + offsetHeight >= scrollHeight - 100) {
-            setIsBottom(true);
-          }
-        }
-      });
+  const handleLike = (url) => {
+    const updatedCatsData = catsData.map((cat) => {
+      if (cat.url === url) {
+        return { ...cat, liked: !cat.liked };
+      } else {
+        return cat;
+      }
     });
-
-    // Observe the second-last cat card element
-    if (secondLastCatCardRef.current) {
-      observer.observe(secondLastCatCardRef.current);
-    }
-
-    // Cleanup function to disconnect observer
-    return () => {
-      observer.disconnect();
-    };
-  }, [secondLastCatCardRef]);
+    setCatsData(updatedCatsData);
+  };
 
   return (
     <div className="cats-list">
-      {catsData &&
-        catsData.map((item, index) => {
-          if (index === catsData.length - 2) {
-            return (
-              <CatCard
-                key={uuidv4()}
-                ref={secondLastCatCardRef}
-                url={item.url}
-                liked={item.liked}
-              />
-            );
-          } else if (index === catsData.length - 1) {
-            return (
-              <CatCard key={uuidv4()} url={item.url} liked={item.liked} />
-            );
-          } else {
-            return (
-              <CatCard key={uuidv4()} url={item.url} liked={item.liked} />
-            );
-          }
-        })}
+      {catsData.map((cat, index) => {
+        if (index === catsData.length - 1) {
+          return (
+            <CatCard
+              key={uuidv4()}
+              ref={lastCatCardRef}
+              url={cat.url}
+              liked={cat.liked}
+              handleLike={handleLike}
+            />
+          );
+        } else {
+          return (
+            <CatCard
+              key={uuidv4()}
+              url={cat.url}
+              liked={cat.liked}
+              handleLike={handleLike}
+            />
+          );
+        }
+      })}
+      {isLoading && <div>Loading...</div>}
     </div>
   );
 }
